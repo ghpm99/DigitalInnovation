acefa020e1b27c1226232a2bd6c04a63d99e5d6c
Guilherme Henrique <ghpm99@hotmail.com>
2021-08-11 11:44:08 -0300

    Cria projeto java

---
+++ src/paradigma/Aula.java
@@ -0,0 +1,14 @@
+package paradigma;
+
+import java.util.function.UnaryOperator;
+
+public class Aula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UnaryOperator<Integer> calcularValorVezesTres = valor -> valor * 3;
+		int valor = 10;
+		System.out.println("O resultado é: " + calcularValorVezesTres.apply(valor));
+	}
+
+}
