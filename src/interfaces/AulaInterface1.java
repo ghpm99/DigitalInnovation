e1f7b6431f2f92298c40fd16dc59c914f31f9337
Guilherme Henrique <ghpm99@hotmail.com>
2021-08-12 12:46:48 -0300

    Atualizacao arquivos!

--- src/interfaces/AulaInterface1.java
+++ src/interfaces/AulaInterface1.java
@@ -2,8 +2,6 @@ package interfaces;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 //aula sobre interface funcional
 public class AulaInterface1 {
